80e243554538677d15640c6bd7e0bcf50855db17
frolushka <mpfrolovstud@gmail.com>
2019-06-15 22:28:29 +0300

    Added working eye tracking

---
+++ Assets/Frolov/Scripts/PointerVisualizer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerVisualizer : MonoBehaviour
+{
+    [SerializeField] private Transform pointer;
+    [SerializeField] private Transform averagedPointer;
+    
+    private void LateUpdate()
+    {
+        averagedPointer.position = EyesInput.Instance.mousePosition;
+        pointer.position = EyesInput.Instance.rawMousePosition;
+        
+        Debug.Log($"av: {averagedPointer.position} ptr: {pointer.position}");
+    }
+}
